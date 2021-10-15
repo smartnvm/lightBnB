329b8ccafdc67fb63bd53295bfed3973bed0a7e9
AJ <aj@smartnvm.com>
2021-10-15 04:49:28 +0000

    - implement custom query filters - code clean up

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,38 +1,28 @@
-const properties = require('./json/properties.json');
-const users = require('./json/users.json');
 const bcrypt = require('bcrypt');
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'lightbnb',
+	user: 'vagrant',
+	password: '123',
+	host: 'localhost',
+	database: 'lightbnb',
 });
 
 /// Users
-
 /**
  * Get a single user from the database given their email.
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  const strQuery =
-    `SELECT * FROM users where email = $1`;
-  return pool
-    .query(strQuery, [email])
-    .then(dBres => {
-      console.log('========== getUserWithEmail ==============');
-      console.log(dBres.rows);
-      return dBres.row;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+	const strQuery = `SELECT * FROM users where email = $1`;
+	return pool
+		.query(strQuery, [email])
+		.then((dBres) => dBres.row)
+		.catch((err) => {
+			console.log(err.message);
+		});
 };
-
-// getUserWithEmail('sebastianguerra@ymail.com');
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -41,24 +31,15 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  const strQuery =
-    `SELECT * FROM users where id = $1`;
+	const strQuery = `SELECT * FROM users where id = $1`;
 
-  return pool
-    .query(strQuery, [id])
-    .then(dBres => {
-      console.log('========== getUserWithId ==============');
-      console.log(dBres.rows);
-      return dBres.row;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-
+	return pool
+		.query(strQuery, [id])
+		.then((dBres) => dBres.row)
+		.catch((err) => {
+			console.log(err.message);
+		});
 };
-// getUserWithId(1);
-
-
 exports.getUserWithId = getUserWithId;
 
 /**
@@ -67,42 +48,28 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  console.log('========== adding user ==============');
-  console.log(user);
-  const { name, email, password } = user;
-  const salt = bcrypt.genSaltSync(10);
+	const { name, email, password } = user;
+	const salt = bcrypt.genSaltSync(10);
 
-  bcrypt.hash(password, salt)
-    .then(function (hash) {
-      // Store hash in your password DB.
-      const sqlQuery =
-        `INSERT INTO
+	bcrypt
+		.hash(password, salt)
+		.then(function (hash) {
+			const sqlQuery = `INSERT INTO
         users (name, email, password)
-        VALUES
-        ($1, $2, $3)
+        VALUES ($1, $2, $3)
         RETURNING *`;
-      return pool
-        .query(sqlQuery, [name, email, hash])
-        .then(dBres => {
-          console.log(dBres.rows);
-          return dBres.rows;
-        });
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-
+			return pool
+				.query(sqlQuery, [name, email, hash])
+				.then((dBres) => dBres.rows);
+		})
+		.catch((err) => {
+			console.log(err.message);
+		});
 };
 
-const name = 'wtf_name';
-const email = 'name@name.com';
-const password = 'password';
-const user = { name, email, password };
-//addUser(user);
 exports.addUser = addUser;
 
 /// Properties
-
 /**
  * Get all properties.
  * @param {{}} options An object containing query options.
@@ -110,27 +77,65 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function (options, limit = 10) {
-  const strQuery =
-    `SELECT * FROM properties 
-    WHERE owner_id = $1
-    LIMIT $2;`;
-  return pool
-    .query(strQuery, [options, limit])
-    .then(dbRes => {
-      console.log('========== getAllProperties ==============');
-      console.log(dbRes.rows);
-      return dbRes.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-  // .finally(() => pool.end());
+	const queryParams = [];
 
+	let strQuery = `
+      SELECT properties.*, avg(property_reviews.rating) as average_rating
+      FROM properties
+      JOIN property_reviews ON property_id = properties.id\n`;
+
+	if (options.city) {
+		queryParams.push(`%${options.city}%`);
+		strQuery += `WHERE city LIKE $${queryParams.length}\n`;
+	}
+
+	if (options.owner_id) {
+		queryParams.push(options.owner_id);
+		if (!strQuery.includes('WHERE')) {
+			strQuery += `WHERE owner_id = $${queryParams.length}\n`;
+		} else {
+			strQuery += `AND owner_id = $${queryParams.length}\n`;
+		}
+	}
+
+	if (options.minimum_price_per_night) {
+		queryParams.push(options.minimum_price_per_night * 100);
+		if (!strQuery.includes('WHERE')) {
+			strQuery += `WHERE cost_per_night >= $${queryParams.length}\n`;
+		} else {
+			strQuery += `AND cost_per_night >= $${queryParams.length}\n`;
+		}
+	}
+
+	if (options.maximum_price_per_night) {
+		queryParams.push(options.maximum_price_per_night * 100);
+		if (!strQuery.includes('WHERE')) {
+			strQuery += `WHERE cost_per_night <= $${queryParams.length}\n`;
+		} else {
+			strQuery += `AND cost_per_night <= $${queryParams.length}\n `;
+		}
+	}
+
+	strQuery += `GROUP BY properties.id \n`;
+
+	if (options.minimum_rating) {
+		queryParams.push(options.minimum_rating);
+		strQuery += `HAVING avg(property_reviews.rating)  >= $${queryParams.length}\n`;
+	}
+
+	queryParams.push(limit);
+	strQuery += `ORDER BY cost_per_night DESC, average_rating DESC 
+              LIMIT $${queryParams.length};`;
+
+	return pool
+		.query(strQuery, queryParams)
+		.then((dbRes) => dbRes.rows)
+		.catch((err) => {
+			console.log(err.message);
+		});
 };
-// getAllProperties(1, 5);
 
 exports.getAllProperties = getAllProperties;
-
 
 /**
  * Add a property to the database
@@ -138,13 +143,25 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const { owner_id, title, description,
-    thumbnail_photo_url, cover_photo_url, cost_per_night,
-    parking_spaces, number_of_bathrooms, number_of_bedrooms,
-    country, street, city, province, post_code, active } = property;
+	const {
+		owner_id,
+		title,
+		description,
+		thumbnail_photo_url,
+		cover_photo_url,
+		cost_per_night,
+		parking_spaces,
+		number_of_bathrooms,
+		number_of_bedrooms,
+		country,
+		street,
+		city,
+		province,
+		post_code,
+		active,
+	} = property;
 
-  const sqlQuery =
-    `INSERT INTO
+	const sqlQuery = `INSERT INTO
       properties ( owner_id,  title,  description,  
         thumbnail_photo_url,  cover_photo_url,  cost_per_night, 
         parking_spaces,  number_of_bathrooms,  number_of_bedrooms,  
@@ -153,54 +170,40 @@ const addProperty = function (property) {
               $10, $11, $12, $13, $14, $15)
       RETURNING *;`;
 
-  const values = [owner_id, title, description,
-    thumbnail_photo_url, cover_photo_url, cost_per_night,
-    parking_spaces, number_of_bathrooms, number_of_bedrooms,
-    country, street, city, province, post_code, active];
+	const values = [
+		owner_id,
+		title,
+		description,
+		thumbnail_photo_url,
+		cover_photo_url,
+		cost_per_night,
+		parking_spaces,
+		number_of_bathrooms,
+		number_of_bedrooms,
+		country,
+		street,
+		city,
+		province,
+		post_code,
+		active,
+	];
 
-  return pool
-    .query(sqlQuery, values)
-    .then(dBres => {
-      console.log('========== addProperty ==============');
-      console.log(dBres.rows);
-      return dBres.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+	return pool
+		.query(sqlQuery, values)
+		.then((dBres) => dBres.rows)
+		.catch((err) => {
+			console.log(err.message);
+		});
 };
-
-temp = {
-  owner_id: 1,
-  title: 'wtf title',
-  description: 'wtf description',
-  thumbnail_photo_url: 'http://thumbnail',
-  cover_photo_url: 'https://cover',
-  cost_per_night: 46058,
-  parking_spaces: 6,
-  number_of_bathrooms: 4,
-  number_of_bedrooms: 8,
-  country: 'Canada',
-  street: '536 Namsub Highway',
-  city: 'Sotboske',
-  province: 'wtf prov',
-  post_code: 28142,
-  active: false,
-};
-// console.log('========== addProperty ==============');
-//addProperty(temp);
 exports.addProperty = addProperty;
 
 /// Reservations
-
 /**
  * Get all reservations for a single user.
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(guest_id, limit);
+	return getAllProperties(guest_id, limit);
 };
-// console.log('========== getAllReservations ==============');
-// getAllReservations(110, 5);
 exports.getAllReservations = getAllReservations;
